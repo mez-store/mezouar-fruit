59cf6663b171cb25d973fec7fc280a0d6e1ed98c
Mezouar Abderrahmane <mezouarabderrahmane04@gmail.com>
2026-01-30 19:06:14 +0100

    fix urls and enable store pages

--- mon_magasin/urls.py
+++ mon_magasin/urls.py
@@ -23,6 +23,7 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('products.urls')),
     path('cart/', include('cart.urls')),
+    path('orders/', include('orders.urls')),
 ]
 
 if settings.DEBUG:
